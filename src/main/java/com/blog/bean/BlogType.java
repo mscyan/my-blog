e0458003845c308ae52dcae47c13e62d76ca711a
yanchao <yanchao@00001@hotmail.com>
2019-04-16 17:16:29 +0800

    删除类文件，重新开发功能。

--- src/main/java/com/blog/bean/BlogType.java
+++
@@ -1,7 +0,0 @@
-package com.blog.bean;
-
-public class BlogType {
-    public final static String MARK_DOWN = "markdown";
-    public final static String HTML = "html";
-    public final static String TEXT = "text";
-}
--- src/main/java/com/blog/service/Imp/TempBlogServiceImp.java
+++
@@ -1,9 +0,0 @@
-package com.blog.service.Imp;
-
-import com.blog.service.TempBlogService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class TempBlogServiceImp implements TempBlogService
-{
-}
