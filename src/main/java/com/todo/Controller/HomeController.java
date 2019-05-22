6315476b7b8bc03e4d0b4fc1525f0f81599ce2e1
yanchao <yanchao@00001@hotmail.com>
2019-05-22 14:29:19 +0800

    commit-20190522

---
+++ src/main/java/com/todo/Controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.todo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("home")
+    @ResponseBody
+    public String home(){
+
+        return "hello world " + new Date();
+    }
+
+    @RequestMapping("/")
+    public String index_default(){
+
+        return "/index.html";
+    }
+
+    @RequestMapping("index")
+    public String index(){
+
+        return "/index.html";
+    }
+}
