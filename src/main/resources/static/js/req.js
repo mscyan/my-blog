3443c13b39f2c94ed58839650a0eeef9c6e511da
yanchao <yanchao00001@hotmail.com>
2019-06-11 00:57:30 +0800

    blog开发开始

--- src/main/resources/static/js/req.js
+++ src/main/resources/static/js/req.js
@@ -10,7 +10,7 @@ function req_post_method(){
 function upload_file(url, method){
     var fileBtn = document.getElementById("file");
 
-    console.log(fileBtn.files[0]);
+    // console.log(fileBtn.files[0]);
     if(fileBtn.files[0] === undefined){
         alert("please select a file");
     }
@@ -22,6 +22,43 @@ function upload_file(url, method){
         else{
             var tailName = totalName.substring(totalName.lastIndexOf('.') + 1, totalName.length);
             if(tailName === 'jpg' || tailName === 'png' || tailName === "jpeg" || tailName === "gif"){
+                var fd = new FormData();
+                fd.append('file', fileBtn.files[0]);
+                var xhr = new XMLHttpRequest();
+                xhr.open(method, url, true);
+                xhr.send(fd);
+                xhr.onreadystatechange = function(){
+                    if(xhr.readyState === 4 && xhr.status === 200){
+                        var response  = JSON.parse(xhr.responseText);
+                        var link = response.response_data;
+                        document.getElementById('review').src = '/pic/getpic/' + link;
+                        document.getElementById("short_link").innerText = '/pic/getpic/' + link;
+                    }
+                }
+            }
+            else{
+                console.log("this file is not suitable");
+            }
+        }
+    }
+}
+
+function upload_blog(url, method){
+    var fileBtn = document.getElementById('contentFile');
+    if(fileBtn.files[0] === undefined){
+        alert('please select a markdown file');
+        return;
+    }
+    else{
+        var title = document.getElementById('title');
+        var abstract = document.getElementById('abstract');
+        var fileTotalName = fileBtn.files[0].name;
+        if(fileTotalName.lastIndexOf('.') === -1){
+            alert("this file is not Suitable");
+        }
+        else{
+            var suffixName = fileTotalName.substring(fileTotalName.lastIndexOf('.') + 1, fileTotalName.length);
+            if(suffixName === 'md'){
                 var fd = new FormData();
                 fd.append('file', fileBtn.files[0]);
                 var xhr = new XMLHttpRequest();
